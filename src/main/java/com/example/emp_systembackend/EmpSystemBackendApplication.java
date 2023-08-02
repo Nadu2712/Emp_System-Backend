9f4cb3bd5d326dccaa9286242b15d3ae26eff02d
Nadushanth Sathiskumar <snadushanth2000@gmail.com>
2023-08-02 12:36:46 +0530

    Initial commit

---
+++ src/main/java/com/example/emp_systembackend/EmpSystemBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.emp_systembackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmpSystemBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EmpSystemBackendApplication.class, args);
+    }
+
+}
